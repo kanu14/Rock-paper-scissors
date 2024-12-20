6b8e96729d60263bc2349df6761d41152d47b2b1
kanu14 <mparkeh14@gmail.com>
2024-12-19 20:17:06 -0800

    add buttons and make them part of human choice

--- javascript.js
+++ javascript.js
@@ -1,69 +1,85 @@
-// Get computer choice randomly choosing r, p, or s
-// Get user choice
-// declare score variables to keep track
-// Play the game
+    // Get computer choice randomly choosing r, p, or s
+    // Get user choice
+    // declare score variables to keep track
+    // Play the game
 
+    const buttons = document.querySelectorAll('button');
 
-function getComputerChoice() {
-    let random = Math.floor(Math.random() * 3 + 1);
-    if (random == 1) {
-        return 'rock';
-    } else if (random == 2) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
-}
-// console.log(getComputerChoice());
-
-function getHumanChoice() {
-    let human = prompt('Enter rock, paper, or scissors');
-    if (human == 'rock' || human == 'paper' || human == 'scissors') {
-        return human;
-    } else {
-        alert('enter again');
-    }
-}
-
-let humanScore = 0;
-let compScore = 0;
-// let n = 0;
-
-function playRound(humanChoice, compChoice) {
-    if (humanChoice == 'rock' && compChoice == 'scissors' || 
-    humanChoice == 'scissors' && compChoice == 'paper' ||
-    humanChoice == 'paper' && compChoice == 'rock') {
-        console.log('You win! ' + humanChoice + ' beats ' + compChoice +'!');
-        humanScore++;
-    } else if (compChoice == 'rock' && humanChoice == 'scissors' || 
-    compChoice == 'scissors' && humanChoice == 'paper' ||
-    compChoice == 'paper' && humanChoice == 'rock') {
-        console.log('You lose! ' + compChoice + ' beats ' + humanChoice +'!');
-        compScore++;
-        }
-    else {
-        console.log('draw!');
-        // n++;
-        }
-}
-
-function playGame () {
-    for (let i = 0; i < 6; i++) {
-
-        if (humanScore == 3 || compScore == 3){
-            console.log('Game ended!')
+    let humanChoice = '';
+    let humanScore = 0;
+    let compScore = 0;
+        
+    function getComputerChoice() {
+        let random = Math.floor(Math.random() * 3 + 1);
+        if (random == 1) {
+            return 'rock';
+        } else if (random == 2) {
+            return 'paper';
         } else {
-            const humanSelection = getHumanChoice();
-            const compSelection = getComputerChoice();
-            playRound(humanSelection, compSelection);
-            console.log('Human ' + humanSelection +': '+ humanScore);
-            console.log('Comp ' + compSelection +': '+ compScore);
+            return 'scissors';
         }
     }
 
-}
 
-playGame();
+    function getHumanChoice() {
 
-// console.log('Human ' + humanSelection +': '+ humanScore);
-// console.log('Comp ' + compSelection +': '+ compScore);
+        buttons.forEach(button => {
+            
+            button.addEventListener('click', () => {
+                // Update the external state variable
+                humanChoice = button.textContent.toLowerCase(); 
+                console.log('User choice: ' + humanChoice);
+                
+                const compChoice = getComputerChoice();
+                console.log('Comp choice: ' + compChoice);
+                // Call playRound immediately after getting the choice
+                playRound(humanChoice, getComputerChoice()); 
+                console.log('Human ' + humanChoice + ': ' + humanScore);
+                console.log('Computer ' + getComputerChoice() + ': ' + compScore);
+            });
+        });
+    }
+
+
+
+    function playRound(humanChoice, compChoice) {
+        if (humanChoice == compChoice) {
+            console.log('draw!');
+        } else if (humanChoice == 'rock' && compChoice == 'scissors' || 
+        humanChoice == 'scissors' && compChoice == 'paper' ||
+        humanChoice == 'paper' && compChoice == 'rock') {
+            console.log('You win! ' + humanChoice + ' beats ' + compChoice +'!');
+            humanScore++;
+        } else {
+            console.log('You lose! ' + compChoice + ' beats ' + humanChoice +'!');
+            compScore++;
+        }
+        
+        // if (humanChoice == 'rock' && compChoice == 'scissors' || 
+        //     humanChoice == 'scissors' && compChoice == 'paper' ||
+        //     humanChoice == 'paper' && compChoice == 'rock') {
+        //     console.log('You win! ' + humanChoice + ' beats ' + compChoice +'!');
+        //     humanScore++;
+        // } else if (compChoice == 'rock' && humanChoice == 'scissors' || 
+        //         compChoice == 'scissors' && humanChoice == 'paper' ||
+        //         compChoice == 'paper' && humanChoice == 'rock') {
+        //     console.log('You lose! ' + compChoice + ' beats ' + humanChoice +'!');
+        //     compScore++;
+        //     }
+        // else {
+        //     console.log('draw!');
+        // }
+    }
+
+    function playGame () {
+        console.log('Make your selection');
+        getHumanChoice();
+
+        
+    }
+
+
+
+    playGame();
+
+
